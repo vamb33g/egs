f31e4f8ed04b0eff9348ec7273664c43b4efead4
vamb33g <fabs.webdev@gmail.com>
2023-12-11 23:22:26 +0100

    done

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -20,9 +20,12 @@ export default function FeaturesBlocks() {
               détente pour chaque client.
             </p>
           </div>
+          <div className="max-w-3xl mx-auto text-center underline mb-10">
+            <h2 className="h2 mb-4">Voir la carte</h2>
+          </div>
 
           {/* Items */}
-          <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
+          <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none shadow-sm">
             {/* 1st item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
               <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Coupe + Barbe</h4>
@@ -33,17 +36,6 @@ export default function FeaturesBlocks() {
                 width={100}
                 height={100}
               />
-              {/* <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <g fill="none" fillRule="evenodd">
-                  <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
-                  <g strokeWidth="2">
-                    <path className="stroke-current text-blue-300" d="M34.514 35.429l2.057 2.285h8M20.571 26.286h5.715l2.057 2.285" />
-                    <path className="stroke-current text-white" d="M20.571 37.714h5.715L36.57 26.286h8" />
-                    <path className="stroke-current text-blue-300" strokeLinecap="square" d="M41.143 34.286l3.428 3.428-3.428 3.429" />
-                    <path className="stroke-current text-white" strokeLinecap="square" d="M41.143 29.714l3.428-3.428-3.428-3.429" />  
-                  </g>
-                </g>
-              </svg> */}
               <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Coupe Seule</h4>
               <p className="text-gray-600 text-center">20min . 17€</p>
             </div>
@@ -59,23 +51,13 @@ export default function FeaturesBlocks() {
                 width={109}
                 height={100}
               />
-              {/* <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <g fill="none" fillRule="evenodd">
-                  <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
-                  <g strokeWidth="2" transform="translate(19.429 20.571)">
-                    <circle className="stroke-current text-white" strokeLinecap="square" cx="12.571" cy="12.571" r="1.143" />
-                    <path className="stroke-current text-white" d="M19.153 23.267c3.59-2.213 5.99-6.169 5.99-10.696C25.143 5.63 19.514 0 12.57 0 5.63 0 0 5.629 0 12.571c0 4.527 2.4 8.483 5.99 10.696" />
-                    <path className="stroke-current text-blue-300" d="M16.161 18.406a6.848 6.848 0 003.268-5.835 6.857 6.857 0 00-6.858-6.857 6.857 6.857 0 00-6.857 6.857 6.848 6.848 0 003.268 5.835" />
-                  </g>
-                </g>
-              </svg> */}
               <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Contour</h4>
               <p className="text-gray-600 text-center">15min . 9€</p>
             </div>
 
             {/* 3rd item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
-            <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Black Masque</h4>
+              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Black Masque</h4>
               <p className="text-gray-600 text-center">30min . 10€</p>
               <Image
                 src={'/images/logobarber.png'}
@@ -83,28 +65,11 @@ export default function FeaturesBlocks() {
                 width={90}
                 height={250}
               />
-
-              {/* <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <g fill="none" fillRule="evenodd">
-                  <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
-                  <g strokeWidth="2">
-                    <path className="stroke-current text-blue-300" d="M34.743 29.714L36.57 32 27.43 43.429H24M24 20.571h3.429l1.828 2.286" />
-                    <path className="stroke-current text-white" strokeLinecap="square" d="M34.743 41.143l1.828 2.286H40M40 20.571h-3.429L27.43 32l1.828 2.286" />
-                    <path className="stroke-current text-blue-300" d="M36.571 32H40" />
-                    <path className="stroke-current text-white" d="M24 32h3.429" strokeLinecap="square" />
-                  </g>
-                </g>
-              </svg> */}
               <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">
                 Shampoing détente
               </h4>
               <p className="text-gray-600 text-center">5min . 3€</p>
             </div>
-
-            
-
-           
-
           </div>
         </div>
       </div>
--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import TestimonialImage from '@/public/images/hck.png'
+import TestimonialImage from '@/public/images/hckkk.png'
 
 export default function Testimonials() {
   return (
@@ -81,7 +81,7 @@ export default function Testimonials() {
                   <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-yellow-500" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
                     <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
                   </svg>
-                  <Image className="relative rounded-full" src={TestimonialImage} width={96} height={96} alt="Testimonial 01" />
+                  <Image className="relative rounded-full" src={TestimonialImage} width={86} height={86} alt="Testimonial 01" />
                 </div>
                 <blockquote className="text-xl font-medium mb-4">
                   “ Ce salon est un incontournable pour tout homme soucieux de son apparence. Simple d'accès, ambiance top, et les coupes sont toujours au point. J'y vais régulièrement et les compliments sur mon look ne cessent d'augmenter. “
